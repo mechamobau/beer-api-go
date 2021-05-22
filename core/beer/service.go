83ffd11c7d65470c332737d25de1ebbfbcae90e6
Lucas Viana <contato@lucasviana.dev>
2021-05-22 19:06:28 -0300

    :sparkles: creates Beer service

---
+++ core/beer/service.go
@@ -0,0 +1,42 @@
+package beer
+
+// define a interface com as funções que serão usadas pelo restante do projeto
+type UseCase interface {
+	GetAll() ([]*Beer, error)
+	Get(ID int64) (*Beer, error)
+	Store(b *Beer) error
+	Update(b *Beer) error
+	Remove(ID int64) error
+}
+
+// Em Go, qualquer coisa que implemente as funções de uma interface passa a
+// ser uma implementação válida. Não existe a palavra "implements" como em Java
+// ou em PHP. Desta forma, uma struct, string, inteiro, etc, qualquer coisa pode
+// ser válido, desde que implemente todas as funções.
+type Service struct{}
+
+// esta função retorna um ponteiro em memória para uma estrutura
+func NewService() *Service {
+	return &Service{}
+}
+
+// vamos implementar as funções na próxima etapa
+func (s *Service) GetAll() ([]*Beer, error) {
+	return nil, nil
+}
+
+func (s *Service) Get(ID int64) (*Beer, error) {
+	return nil, nil
+}
+
+func (s *Service) Store(b *Beer) error {
+	return nil
+}
+
+func (s *Service) Update(b *Beer) error {
+	return nil
+}
+
+func (s *Service) Remove(ID int64) error {
+	return nil
+}
